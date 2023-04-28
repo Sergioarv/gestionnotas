0dddda33db658e8ecb07a34e1d6031b5164c8f9e
SERGIO RODRÍGUEZ <SARV9208@hotmail.com>
2023-04-28 11:35:24 -0500

    Se agrega elk proyecto

---
+++ src/main/java/com/natalia/gestionnotas/GestionnotasApplication.java
@@ -0,0 +1,13 @@
+package com.natalia.gestionnotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natalia/gestionnotas/GestionnotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natalia.gestionnotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
